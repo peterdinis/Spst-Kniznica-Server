e61ecdeaee571280ed354e6ed0cfacf79b6e34f5
Peterdinis <peterdinis0@pobox.sk>
2022-06-25 13:21:02 +0200

    Adding new modules to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
+import { BookModule } from './book/book.module';
+import { BorrowingModule } from './borrowing/borrowing.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule]
+  imports: [
+    PrismaModule, 
+    UserModule,
+    AuthModule, 
+    AdminModule,
+    BookModule,
+    BorrowingModule
+  ],
 })
 export class AppModule {}
