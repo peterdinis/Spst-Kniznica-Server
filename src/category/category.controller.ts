e7e324ab49ee97858322ae861e0fa7cd52d5bc32
Peterdinis <peterdinis0@pobox.sk>
2022-06-25 22:19:57 +0200

    New category module

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,59 @@
-import { Controller } from "@nestjs/common";
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+    Query,
+  } from '@nestjs/common';
+import {CategoryService} from "./category.service"
+import {CreateCategoryDto} from "./dto/create-category.dto"
+import {UpdateCategoryDto} from "./dto/update-category.dto";
+import {ApiTags, ApiOperation, ApiOkResponse, ApiCreatedResponse} from "@nestjs/swagger";
 
+@ApiTags("Categories")
 @Controller("category")
-export class CategoryController {}
+export class CategoryController {
+    constructor(private readonly categoriesService: CategoryService) { }
+
+    @ApiOperation({
+        summary: "All Categories",
+    })
+    @ApiOkResponse()
+    @Get("/")
+    findAll() {
+        return this.categoriesService.allCategories();
+    }
+
+    @ApiOperation({
+        summary: "Create category"
+    })
+    @ApiCreatedResponse()
+    @Post("/")
+    createCategory(@Body() createData: CreateCategoryDto) {
+        return this.categoriesService.create(createData)
+    }
+
+    @ApiOperation({
+        summary: "Update category"
+    })
+    @ApiOkResponse()
+    @Patch("/:id")
+    updateCategory(
+        @Param("id") id: string,
+        @Body() updateData: UpdateCategoryDto
+    ) {
+        return this.categoriesService.update(id, updateData);
+    }
+
+    @ApiOperation({
+        summary: "Delete category"
+    })
+    @ApiOkResponse()
+    @Delete("/:id")
+    deleteCategory(@Param("id") id: string) {
+        return this.categoriesService.remove(id);
+    }
+}
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from "@nestjs/common";
-
+import { Injectable, Logger } from '@nestjs/common';
+import {PrismaService} from "../prisma/prisma.service";
+import {CreateCategoryDto} from "./dto/create-category.dto"
+import {UpdateCategoryDto} from "./dto/update-category.dto"
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+    private logger = new Logger(CategoryService.name);
+
+    constructor(private prismaService: PrismaService) { 
+
+    }
+
+    async create(createCategoryDto: CreateCategoryDto) {
+        const result = await this.prismaService.category.create({ data: createCategoryDto });
+        this.logger.log(`Category has been created : ${JSON.stringify(result)}`)
+        return result
+    }
+
+    async allCategories() {
+        const result = await this.prismaService.category.findMany();
+        return result;
+    }
+
+    findOne(id: string) {
+        return this.prismaService.category.findUnique({ where: { id } });
+      }
+    
+      async update(id: string, updateCategoryDto: UpdateCategoryDto) {
+        const result = await this.prismaService.category.update({
+          data: updateCategoryDto,
+          where: { id },
+        });
+        this.logger.warn(`Category has been updated : ${JSON.stringify(result)}`)
+        return result
+      }
+    
+      async remove(id: string) {
+        const result = await this.prismaService.category.delete({ where: { id } });
+        this.logger.warn(`Category has been deleted : ${JSON.stringify(result)}`)
+        return result
+      }
+}
