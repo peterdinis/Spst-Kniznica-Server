d279758872ec7d65216a49bd53586ba3d34cf139
Peterdinis <peterdinis0@pobox.sk>
2022-06-25 12:51:36 +0200

    Adding new modules to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule]
+  imports: [PrismaModule, UserModule, AuthModule]
 })
 export class AppModule {}
---
+++ src/user/dto/query.dto.ts
@@ -0,0 +1,3 @@
+export class UserQueryDto {
+    borrowedBooks?: boolean = false;
+}
