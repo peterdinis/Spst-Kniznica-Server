dcb2b321040b24dc6325d764655cf66504b9caf3
Peterdinis <peterdinis0@pobox.sk>
2022-06-25 23:11:27 +0200

    Working on book module

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,10 @@
-import { Injectable } from "@nestjs/common";
-
+import { Injectable, Logger } from '@nestjs/common';
+import {PrismaService} from "../prisma/prisma.service";
 @Injectable()
-export class BookService {}
+export class BookService {
+    private logger = new Logger(BookService.name);
+
+    constructor(private prismaService: PrismaService) { 
+
+    }
+}
