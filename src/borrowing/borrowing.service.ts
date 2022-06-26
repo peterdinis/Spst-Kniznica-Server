0f1df760e59e578e3fd15a7a9ae7ec9eca3e0345
Peterdinis <peterdinis0@pobox.sk>
2022-06-26 10:43:39 +0200

    Start working on borrowing module

--- src/borrowing/borrowing.service.ts
+++ src/borrowing/borrowing.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from "@nestjs/common";
-
+import { Injectable, Logger, BadRequestException } from '@nestjs/common';
+import { UpdateQuantityDto } from './dto/card-dto';
+import {PrismaService} from "../prisma/prisma.service";
 @Injectable()
-export class BorrowingService {}
+export class BorrowingService {
+    private logger = new Logger(BorrowingService.name);
+
+    constructor(private prismaService: PrismaService) { }
+
+
+}
