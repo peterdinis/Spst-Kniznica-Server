5041fabf89dd0d9a43a7bce905a5619bec1281ea
Peterdinis <peterdinis0@pobox.sk>
2022-06-26 11:25:44 +0200

    FOO

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
 import { BookModule } from './book/book.module';
-import { BorrowingModule } from './borrowing/borrowing.module';
 import { CategoryModule } from './category/category.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
@@ -14,7 +13,6 @@ import { UserModule } from './user/user.module';
     AuthModule, 
     AdminModule,
     BookModule,
-    BorrowingModule,
     CategoryModule
   ],
 })
