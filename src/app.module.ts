1d1ce4618455773e88193e15b302142d059fc925
Peterdinis <peterdinis0@pobox.sk>
2022-07-02 17:25:49 +0200

    Adding new module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,17 @@ import { BookModule } from './book/book.module';
 import { CategoryModule } from './category/category.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import {UserModule } from './user/user.module';
+import {ScheduleModule} from "@nestjs/schedule";
+import {BorrowingModule} from './borrowing/borrowing.module'
 
 @Module({
   imports: [
     CategoryModule,
     BookModule,
     EventEmitterModule.forRoot(),
-    UserModule
+    ScheduleModule.forRoot(),
+    UserModule,
+    BorrowingModule
   ],
 })
 export class AppModule {}
