22638b5276712ad558e7393b041802bf78f6b450
Peterdinis <peterdinis0@pobox.sk>
2022-07-02 15:48:22 +0200

    Update sequelize config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AdminModule } from './admin/admin.module';
 import { BookModule } from './book/book.module';
 import { CategoryModule } from './category/category.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,17 +1,22 @@
 import { Sequelize } from 'sequelize-typescript';
 import { Book } from 'src/book/book.entity';
 import { Category } from 'src/category/category.entity';
-import { ConfigService } from './config.service';
 
 export const databaseProviders = [
     {
         provide: 'SEQUELIZE',
-        useFactory: async (configService: ConfigService) => {
-            const sequelize = new Sequelize(configService.sequelizeOrmConfig);
-           sequelize.addModels([Book, Category]);
-            await sequelize.sync();
-            return sequelize;
+        useFactory: async () => {
+          const sequelize = new Sequelize({
+            dialect: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'password',
+            database: 'nest',
+          });
+          sequelize.addModels([Book, Category]);
+          await sequelize.sync();
+          return sequelize;
         },
-        inject: [ConfigService],
-    },
+      },
 ]
