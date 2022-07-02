2ad725e5ec0e7c611064983dbe3d6d1c7d375fdc
Peterdinis <peterdinis0@pobox.sk>
2022-07-02 20:53:12 +0200

    FOO

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,7 +2,3 @@ import { Controller } from "@nestjs/common";
 
 @Controller("users")
 export class UserController {}
-/* 
-https://www.youtube.com/watch?v=ZPYemiWmphM&list=PL0FGkDGJQjJH8uKAWtvvd1aI2Poo-TXGD&index=9
-17:48
-*/
