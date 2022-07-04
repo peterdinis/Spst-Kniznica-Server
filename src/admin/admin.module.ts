2f1891c642d5bff2f400af318baf5bd61b96a0f7
Peterdinis <peterdinis0@pobox.sk>
2022-07-04 21:24:14 +0200

    Start adding casl to app

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AdminController } from "./admin.controller";
 import { AdminService } from "./admin.service";
+import { CaslAbilityFactory } from "./casl/casl-ability.factory";
 
 @Module({
     controllers: [AdminController],
-    providers: [AdminService]
+    providers: [AdminService, CaslAbilityFactory],
+    exports: [CaslAbilityFactory]
 })
 
 export class AdminModule {}
