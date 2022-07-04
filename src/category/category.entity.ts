fd0a4b2743b2110a39fccfbb413d41aa15cc4b65
Peterdinis <peterdinis0@pobox.sk>
2022-07-04 22:15:12 +0200

    Update entities

--- src/category/category.entity.ts
+++ src/category/category.entity.ts
@@ -4,19 +4,13 @@ import {
     AutoIncrement,
     Column,
     DataType,
-    Model,
-    CreatedAt,
-    UpdatedAt,
-    DeletedAt,
+    Model
 } from 'sequelize-typescript';
 
 interface ICategory {
     id: number;
     name: string;
     description: string;
-    createdAt: Date;
-    updatedAt: Date;
-    deletedAt: Date;
 }
 @Table({
     tableName: "categories"
@@ -33,16 +27,4 @@ export class Category extends Model<ICategory> {
     
     @Column
     description: string;
-
-    @CreatedAt
-    @Column({ field: 'created_at' })
-    createdAt: Date;
-
-    @UpdatedAt
-    @Column({ field: 'updated_at' })
-    updatedAt: Date;
-
-    @DeletedAt
-    @Column({ field: 'deleted_at' })
-    deletedAt: Date;
 }
