a3f34e469d2a1a593923cf2aad62f133df2c5153
Peterdinis <peterdinis0@pobox.sk>
2022-07-16 11:06:26 +0200

    Fixing bug in health controller

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -2,37 +2,30 @@ import { Controller, Get } from '@nestjs/common';
 import {
   HealthCheckService,
   HealthCheck,
-  SequelizeHealthIndicator,
   MemoryHealthIndicator,
-  DiskHealthIndicator,
 } from '@nestjs/terminus';
-import {ApiTags} from "@nestjs/swagger";
- 
-@ApiTags("Application healt status")
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Application healt status')
 @Controller('health')
 class HealthController {
   constructor(
     private healthCheckService: HealthCheckService,
-    private sequelizeHealthIndicator: SequelizeHealthIndicator,
     private memoryHealthIndicator: MemoryHealthIndicator,
-    private diskHealthIndicator: DiskHealthIndicator
   ) {}
- 
+
   @Get()
   @HealthCheck()
   check() {
     return this.healthCheckService.check([
-      () => this.sequelizeHealthIndicator.pingCheck('database'),
       // the process should not use more than 300MB memory
-      () => this.memoryHealthIndicator.checkHeap('memory heap', 300 * 1024 * 1024),
+      () =>
+        this.memoryHealthIndicator.checkHeap('memory heap', 300 * 1024 * 1024),
       // The process should not have more than 300MB RSS memory allocated
-      () => this.memoryHealthIndicator.checkRSS('memory RSS', 300 * 1024 * 1024),
-      // the used disk storage should not exceed the 50% of the available space
-      () => this.diskHealthIndicator.checkStorage('disk health', {
-        thresholdPercent: 0.5, path: '/'
-      })
+      () =>
+        this.memoryHealthIndicator.checkRSS('memory RSS', 300 * 1024 * 1024),
     ]);
   }
 }
- 
+
 export default HealthController;
