aefaa35fe9828aa56b0151c44f0bd2959c5404ab
Peterdinis <peterdinis0@pobox.sk>
2022-07-17 13:52:54 +0200

    FOO

--- src/teachers/teachers.controller.ts
+++ src/teachers/teachers.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { ApiBearerAuth, ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { CreateTeacherDto } from "./dto/create-teacher.dto";
+import { TeacherLoginRequestDto } from "./dto/teacher-login-request.dto";
+import { TeachersService } from "./teachers.service";
 
+@ApiTags("Teachers")
 @Controller("teachers") 
-export class TeachersController {}
+export class TeachersController {
+    constructor(private readonly teachersService: TeachersService) {}
+
+    @ApiOperation({
+        summary: "Register new teacher"
+    })
+    @Post("/register")
+    @ApiCreatedResponse()
+    register(@Body() createTeacherDto: CreateTeacherDto) {
+        return this.teachersService.create(createTeacherDto);
+    }
+
+
+    @ApiOperation({
+        summary: "Teacher login to app"
+    })
+    @ApiCreatedResponse()
+    login(
+        @Body() teacherLoginRequestDto: TeacherLoginRequestDto
+    ) {
+        return this.teachersService.login(teacherLoginRequestDto);
+    }
+
+
+    @ApiOperation({
+        summary: "Finding all teachers"
+    })
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard('jwt'))
+    @ApiOkResponse()
+    @Get()
+    findAll() {
+        return this.teachersService.findAll();
+    }
+
+    /* @ApiOperation({
+        summary: "My Teacher Profile"
+    })
+    @Get("me")
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard('jwt'))
+    @ApiOkResponse()
+    async getTeacher(
+        @Req() request
+    ) {
+        return this.teachersService.getTeacher(request.user.id)
+    } */
+
+}
--- src/teachers/teachers.module.ts
+++ src/teachers/teachers.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import {TeachersService} from "./teachers.service"
 import {TeachersController} from "./teachers.controller"
+import { teacherProviders } from './teachers.providers';
+import { DatabaseModule } from '../database/database.module';
+import { JwtStrategy } from './auth/jwt-strategy';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [TeachersController],
-  providers: [TeachersService],
+  providers: [TeachersService, ...teacherProviders, JwtStrategy],
 })
 export class TeachersModule {}
