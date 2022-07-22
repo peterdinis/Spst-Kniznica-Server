eebdd0cf0d9b121fe27cc312db40444eb7924fc1
Peterdinis <peterdinis0@pobox.sk>
2022-07-23 00:38:42 +0200

    FOO

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { DatabaseModule } from "../database/database.module";
 import { AdminController } from "./admin.controller";
-import { AdminGateway } from "./admin.gateway";
 import { adminProviders } from "./admin.providers";
 import { AdminService } from "./admin.service";
 import { JwtStrategy } from "./auth/jwt-strategy";
@@ -9,7 +8,7 @@ import { JwtStrategy } from "./auth/jwt-strategy";
 @Module({
     imports: [DatabaseModule],
     controllers: [AdminController],
-    providers: [AdminGateway, AdminService, ...adminProviders, JwtStrategy],
+    providers: [AdminService, ...adminProviders, JwtStrategy],
     exports: [AdminGateway]
 })
 
