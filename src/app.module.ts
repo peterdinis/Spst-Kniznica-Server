404d6f2e3439b2bd648ad4ed587492337fb4bb7a
Peterdinis <peterdinis0@pobox.sk>
2022-07-23 12:11:40 +0200

    Start working on admin chat

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,8 @@ import { BorrowingModule } from './borrowing/borrowing.module';
 import { HealthModule } from './health/health.module';
 import { CaslModule } from './common/casl/casl.module';
 import { TeachersModule } from './teachers/teachers.module';
-import {ClsModule} from "nestjs-cls"
+import { ClsModule } from 'nestjs-cls';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import {ClsModule} from "nestjs-cls"
       isGlobal: true,
       envFilePath: '.env',
     }),
+    ChatModule,
     CategoryModule,
     BookModule,
     EventEmitterModule.forRoot(),
---
+++ src/common/helpers/redisAdapter.ts
@@ -0,0 +1,3 @@
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import * as redisIoAdapter from 'socket.io-redis';
+import { NestExpressApplication } from '@nestjs/platform-express';
